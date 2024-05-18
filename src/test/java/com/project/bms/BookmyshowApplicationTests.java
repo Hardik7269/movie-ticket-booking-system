eb68ff83e5046983ac23dce2bde994b8e4bd501e
Hardik7269 <hardiklimbachiya9898@gmail.com>
2024-05-19 05:06:53 +0530

    maven structure

---
+++ src/test/java/com/project/bms/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
