d89e542a4cd23bf12d95977d6971d9a1469c59e2
Hardik7269 <hardiklimbachiya9898@gmail.com>
2024-05-27 22:36:24 +0530

    box-office collection

--- src/main/java/com/project/bms/controllers/MovieController.java
+++ src/main/java/com/project/bms/controllers/MovieController.java
@@ -2,6 +2,8 @@ package com.project.bms.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,11 @@ public class MovieController {
 	
 	protected MovieService movieService;
 	
+	public MovieController(MovieService movieService) {
+		super();
+		this.movieService = movieService;
+	}
+
 	@PostMapping("/addMovie")
 	public ResponseEntity<String> addMovie(@RequestBody MovieDto movieReqDto){
 		try {
@@ -21,6 +28,16 @@ public class MovieController {
 			return ResponseEntity.status(HttpStatus.CREATED).body(result);
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.toString());
+		}
+	}
+	
+	@GetMapping("/box-office/{movieId}")
+	public ResponseEntity<String> movieRevenue(@PathVariable Integer movieId){
+		try {
+			String result = movieService.movieRevenue(movieId);
+			return ResponseEntity.ok(result);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
 	}
 }
--- src/main/java/com/project/bms/service/MovieService.java
+++ src/main/java/com/project/bms/service/MovieService.java
@@ -1,11 +1,19 @@
 package com.project.bms.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.project.bms.Repository.MovieRepository;
 import com.project.bms.dtos.MovieDto;
 import com.project.bms.entity.Movie;
+import com.project.bms.entity.Show;
+import com.project.bms.entity.ShowSeat;
+import com.project.bms.exceptations.MovieNotFoundExceptation;
 import com.project.bms.transformDtoTo.TransformDto;
 
 @Service
@@ -24,5 +32,26 @@ public class MovieService {
 		Movie m = transform.movieDtoToMovie(movieDto);
 		movieRepository.save(m);
 		return "Movie Successfully Added !!";
+	}
+	
+	public String movieRevenue(Integer id) throws MovieNotFoundExceptation {
+		Optional<Movie> movieOpt = movieRepository.findById(id);
+		if(movieOpt.isEmpty()) {
+			throw new MovieNotFoundExceptation();
+		}
+		Movie movie = movieOpt.get();
+		Integer revenue = 0;
+		List<Show> showList = movie.getShows();
+		for(Show show : showList) {
+			Predicate<? super ShowSeat> predicate = s -> s.isAvailable()==false;
+			List<Integer> collect = show.getShowSeats().stream().filter(predicate).map(seat -> seat.getPrice()).collect(Collectors.toList());
+			for(Integer n : collect) {
+				revenue += n;
+			}
+		}
+		
+		String movieName = movieRepository.findById(id).get().getMovieName();
+		String response = "Total Collection of Movie "+ movieName + " is : " + revenue; 
+		return response;
 	}
 }
